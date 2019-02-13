a2cc114786586152e2b19642a9ed3c29215ca770
Aman <aman.agarwal@gradeup.co>
2019-02-13 17:21:34 +0530

    feat(fixes): dependency fixes

--- dist/helpers/default-pop-style.js
+++ dist/helpers/default-pop-style.js
@@ -1,13 +1,18 @@
-const defaultPopStyle = {
-	position: 'absolute',
-	background: '#333',
-	color: '#fff',
-	padding: '3px',
-	align: 'center',
-	fontSize: '12px',
-	transform: 'translateX(-50%)',
-	left: '50%',
-	transition: 'all 0.3s ease'
-};
+"use strict";
 
-export { defaultPopStyle };
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.defaultPopStyle = void 0;
+var defaultPopStyle = {
+  position: 'absolute',
+  background: '#333',
+  color: '#fff',
+  padding: '3px',
+  align: 'center',
+  fontSize: '12px',
+  transform: 'translateX(-50%)',
+  left: '50%',
+  transition: 'all 0.3s ease'
+};
+exports.defaultPopStyle = defaultPopStyle;
